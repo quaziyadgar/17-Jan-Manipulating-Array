785ed586080da6f887d7fdb9cc009ead3c574de8
quaziyadgar <quaziyadgar@github.com>
2022-01-18 22:18:52 +0530

    anagram done

--- anagram.js
+++ anagram.js
@@ -2,21 +2,23 @@
 function anagram(str1, str2){
     var l1 = str1.length;
     var l2 = str2.length;
+    
     if(l1!=l2)
     return false;
 
-    str1.sort();
-    str2.sort();
-
-    for( let i = 0 ; i < l1 ; i ++)
+    let a = str1.split('').sort().join('');
+    let b = str2.split('').sort().join('');
+    // console.log(a);
+    // console.log(b);
+    for( let i = 0 ; i <l1; i ++)
     {
-        if(str1[i]!=str2[i])
+        if(a[i]!=b[i])
         return false;
     }
     return true;
 }
-var a = "madan";
-var b = "daman";
+var a = 'namad';
+var b = 'daman';
 if(anagram(a,b))
 console.log("Anagram string");
 else
